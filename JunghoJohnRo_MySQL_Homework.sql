e38895a7a9eac486f55306eb829966216eb7c78c
JunghoJohnRo <junghojohnro@gmail.com>
2019-04-17 11:47:27 -0500

    Updated Version

--- JunghoJohnRo_MySQL_Homework.sql
+++ JunghoJohnRo_MySQL_Homework.sql
@@ -1,107 +1,153 @@
 USE sakila;
 
-# 1a.
+-- 1a.
 SELECT first_name, last_name FROM actor;
 
-# 1b.
+-- 1b.
 SELECT CONCAT(actor.first_name, ' ', actor.last_name) AS `Actor Name` FROM actor;
 
-# 2a.
+-- 2a.
 SELECT * FROM actor WHERE actor.first_name = "Joe";
 
-# 2b.
+-- 2b.
 SELECT * FROM actor WHERE actor.last_name LIKE "%GEN%";
 
-# 2c.
+-- 2c.
 SELECT * FROM actor WHERE actor.last_name LIKE "%LI%" 
 ORDER BY last_name ASC, first_name ASC;
 
-# 2d.
+-- 2d.
 SELECT country_id, country FROM country WHERE country.country 
 IN ("Afghanistan", "Bangladesh", "China");
 
-# 3a.
+-- 3a.
 ALTER TABLE actor ADD COLUMN description BLOB;
-# Checking if Column was added 
-DESCRIBE actor;
 
-# 3b.
+-- 3b.
 ALTER TABLE actor DROP COLUMN description; 
-# Checking if Column was deleted
-DESCRIBE actor;
 
-# 4a.
+-- 4a.
 SELECT last_name, COUNT(last_name) FROM actor GROUP BY last_name;
 
-# 4b.
+-- 4b.
 SELECT last_name, COUNT(last_name) FROM actor GROUP BY last_name
-HAVING COUNT(last_name) > 1;
+HAVING COUNT(last_name) > 2;
 
-# 4c.
+-- 4c.
 SELECT * FROM actor WHERE first_name="GROUCHO" AND last_name="WILLIAMS";
 UPDATE actor SET first_name="HARPO" WHERE actor_id=172; 
-# Check to see if record was changed
-SELECT * FROM actor WHERE actor_id=172;
 
-# 4d.
+-- 4d.
 UPDATE actor SET first_name="GROUCHO" WHERE actor_id=172;
-# Check to see if record was changed
-SELECT * FROM actor WHERE actor_id=172;
 
-# 5a.
+-- 5a.
 SHOW CREATE TABLE address;
 
-# 6a.
+-- 6a.
 SELECT first_name, last_name, address 
 FROM staff
 INNER JOIN address ON staff.address_id = address.address_id; 
 
-# 6b.
+-- 6b.
 SELECT first_name, last_name, SUM(payment.amount) 
 FROM staff
 INNER JOIN payment on staff.staff_id = payment.staff_id
 WHERE payment.payment_date LIKE '2005-08%'
 GROUP BY staff.staff_id;
 
-# 6c.
+-- 6c.
 SELECT title, COUNT(film_actor.actor_id) 
 FROM film
 INNER JOIN film_actor ON film.film_id = film_actor.film_id
 GROUP BY film.title;
 
-# 6d.
+-- 6d.
 SELECT title, COUNT(inventory.inventory_id) 
 FROM film
 INNER JOIN inventory ON film.film_id=inventory.film_id
 WHERE film.title="Hunchback Impossible";
 
-# 6e.
+-- 6e.
 SELECT first_name, last_name, SUM(payment.amount)
 FROM customer
 INNER JOIN payment on customer.customer_id = payment.customer_id
 GROUP BY customer.customer_id
 ORDER BY last_name ASC;
 
-# 7a.
+-- 7a.
 SELECT title 
 FROM film
 INNER JOIN language on film.language_id = language.language_id
 WHERE language.name =  "English" 
 AND film.title LIKE "%K" OR film.title LIKE "%Q";
 
-# 7b.
-SELECT first_name, last_name
-FROM actor
-INNER JOIN film
-# 7c.
-# 7d.
-# 7e.
-# 7f.
-# 7g.
-# 7h.
-# 8a.
-# 8b.
-# 8c.
+-- 7b.
+SELECT first_name, last_name 
+FROM actor 
+WHERE actor.actor_id IN (SELECT film_actor.actor_id FROM film_actor WHERE film_actor.film_id IN 
+(SELECT film.film_id FROM film WHERE title = "Alone Trip"));
 
+-- 7c.
+SELECT first_name, last_name, email
+FROM customer
+WHERE customer.address_id IN (SELECT address.address_id FROM address WHERE address.city_id IN
+(SELECT city.city_id FROM city INNER JOIN country WHERE city.country_id=country.country_id AND 
+country.country="Canada"));
 
+-- 7d.
+SELECT title, category
+FROM film_list
+WHERE category = "Family";
 
+-- 7e. 
+SELECT title, COUNT(film.title) AS "Total Rented"
+FROM film
+INNER JOIN inventory ON inventory.film_id = film.film_id
+INNER JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY film.title
+ORDER BY COUNT(film.title) DESC;
+
+-- 7f.
+SELECT store.store_id, SUM(payment.amount) AS "Revenue"
+FROM store
+INNER JOIN inventory ON store.store_id = inventory.store_id
+INNER JOIN rental ON inventory.inventory_id = rental.inventory_id
+INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY store_id;
+
+-- 7g.
+SELECT store.store_id, city.city, country.country
+FROM store
+INNER JOIN address ON store.address_id = address.address_id
+INNER JOIN city on address.city_id = city.city_id
+INNER JOIN country on city.country_id = country.country_id
+GROUP BY store.store_id;
+
+-- 7h.
+SELECT category.name, SUM(payment.amount) AS "Revenue"
+FROM category
+INNER JOIN film_category ON category.category_id = film_category.category_id
+INNER JOIN inventory on film_category.film_id = inventory.film_id
+INNER JOIN rental on inventory.inventory_id = rental.inventory_id
+INNER JOIN payment on rental.rental_id = payment.rental_id
+GROUP BY category.name
+ORDER BY SUM(payment.amount) DESC
+LIMIT 5;
+
+-- 8a.
+CREATE VIEW Top_Grossing_Genres AS 
+SELECT category.name, SUM(payment.amount) AS "Revenue"
+FROM category
+INNER JOIN film_category ON category.category_id = film_category.category_id
+INNER JOIN inventory on film_category.film_id = inventory.film_id
+INNER JOIN rental on inventory.inventory_id = rental.inventory_id
+INNER JOIN payment on rental.rental_id = payment.rental_id
+GROUP BY category.name
+ORDER BY SUM(payment.amount) DESC
+LIMIT 5;
+
+-- 8b.
+SELECT * FROM Top_Grossing_Genres;
+
+-- 8c.
+DROP VIEW Top_Grossing_Genres;
